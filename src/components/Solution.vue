35c922dc63555194cc777fa96ff7eae4cad08880
kiseiju04 <gabrielemorelli95@gmail.com>
2025-02-14 14:45:04 +0100

    fix

--- src/components/Solution.vue
+++ src/components/Solution.vue
@@ -8,24 +8,28 @@ function redirect() {
 </script>
 
 <template>
-  <a
-      class="link"
-      :href="`https://drive.google.com/file/d/${file.id}/view?usp=sharing`"
-      target="_blank"
-  >{{ file.name }}</a>
-  <a
-      class="link sol"
-      v-if="link !== undefined"
-      @click="redirect"
-      target="_blank
+  <div class="sol-link">
+    <a
+        class="link"
+        :href="`https://drive.google.com/file/d/${file.id}/view?usp=sharing`"
+        target="_blank"
+    >{{ file.name }}</a>
+    <a
+        class="link sol"
+        v-if="link !== undefined"
+        @click="redirect"
+        target="_blank
   ">sol.</a>
+  </div>
 </template>
 
 <style scoped>
 .link {
   color: white;
   text-decoration: none;
+}
 
+.sol-link {
   position: relative;
   z-index: 0;
 }
@@ -34,7 +38,7 @@ function redirect() {
   z-index: 0;
   grid-column: 1 / 2;
   position: absolute;
-  right: 2em;
+  right: -3em;
   cursor: pointer;
 }
 </style>
